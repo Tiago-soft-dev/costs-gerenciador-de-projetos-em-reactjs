a7d4de63fbdf795d7648c11bded7ea2c96f1f364
tiago-soft-dev <tiago.soft.dev@gmail.com>
2024-11-02 01:10:40 +0000

    Estruturação do formuário do projeto

--- costs/src/components/pages/NewProject.jsx
+++ costs/src/components/pages/NewProject.jsx
@@ -1,3 +1,13 @@
+import { ProjectForm } from '../project/ProjectForm'
+import style from './NewProject.module.css'
+
 export function NewProject(){
-  return <h1>New Project</h1>
+  return (
+    <div className={style.container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto e adicione os serviços</p>
+      <ProjectForm />
+    
+    </div>
+  )
 }
---
+++ costs/src/components/project/ProjectForm.jsx
@@ -0,0 +1,27 @@
+import style from './ProjectForm.module.css'
+
+export function ProjectForm(){
+  return (
+    <div className={style.container}>
+      <form>
+        <div>
+         <input type='text' placeholder='Nome do projeto...'/>
+        </div>
+        <div>
+         <input type='number' placeholder='Orçamento do projeto...'/>
+        </div>
+
+        <div>
+          <select name='category_id'>
+            <option disabled>Selecione a categoria</option>
+          </select>
+        </div>
+
+        <div>
+          <input type='submit' value='Criar'/>
+        </div>
+      
+      </form>
+    </div>
+      )
+}
