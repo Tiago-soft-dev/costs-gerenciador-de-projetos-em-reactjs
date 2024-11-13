ff8e9cbd2d147e8478215eea67c15e18084ebb0f
Tiago-soft-dev <tiago.soft.dev@gmail.com>
2024-11-12 21:10:48 -0300

    Conectando com a API

--- costs/src/components/project/ProjectForm.jsx
+++ costs/src/components/project/ProjectForm.jsx
@@ -1,5 +1,5 @@
-
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Button } from '../form/Button'
 import Input from '../form/Input'
 import Select from '../form/Select'
@@ -11,18 +11,54 @@ export function ProjectForm({btnText}){
 
  const [categories, setCategories] = useState([]) 
 
+  // try {
+  //   const response = async () => {
+  //    await axios('http://localhost:5000/categories')
+    
+  //   setCategories(response.data)
+  //   }
 
+    
 
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
-
-
+  // fetch('http://localhost:5000/categories', {
+  //   method: 'get',
+  //   headers: {
+  //     'Content-Type' : 'application/json',
+  //   },
+  // })
+    
+  useEffect(()=>{
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content--type' : 'aplication/json',
+      },
+    })
+    .then((resp) => resp.json())
+    .then((data)=>{
+      setCategories(data)
+    })
+    .catch((err)=>console.log(err))
+  }, [])
 
   return (
     <div className={styles.container}>
       <form>
-        <Input type='text' text='Nome do projeto' name='name' placeholder='Nome do projeto...'/>
+        <Input 
+          type='text' 
+          text='Nome do projeto' 
+          name='name' 
+          placeholder='Nome do projeto...'/>
         
-        <Input type='number' text='Orçamento do projeto' placeholder='Orçamento do projeto...' name='budget'/>
+        <Input 
+          type='number' 
+          text='Orçamento do projeto' 
+          placeholder='Orçamento do projeto...' 
+          name='budget'/>
         
 
         {/* <div>
@@ -31,7 +67,11 @@ export function ProjectForm({btnText}){
           </select>
         </div> */}
 
-        <Select name='category_id' text='Selecione a categoria'/>
+        <Select 
+         name='category_id' 
+         text='Selecione a categoria'
+         options={categories}
+         />
 
         
         
